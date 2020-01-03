7635e93dcddd50fc15609fec978ab061cf71380b
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-01-03 21:22:58 +0900

    Add Gyms to App Index and Action and View Pages

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,6 +1,10 @@
 class GymsController < ApplicationController
-
+  before_action :find_gym, only: [:show, :edit, :update, :destroy]
   def index
+    @gyms = Gym.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -9,11 +13,21 @@ class GymsController < ApplicationController
 
   def create
     @gym = Gym.new(gym_params)
+
+    if @gym.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
 
   def gym_params
     params.require(:gym).permit(:name, :description, :number, :address)
+  end
+
+  def find_gym
+    @gym = Gym.find(params[:id])
   end
 end
