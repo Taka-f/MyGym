b399de5a100b14c73a450846debc3beb80402e9d
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-01-03 19:18:07 +0900

    Add New and Create Actions with View Files

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,4 +2,18 @@ class GymsController < ApplicationController
 
   def index
   end
+
+  def new
+    @gym = Gym.new
+  end
+
+  def create
+    @gym = Gym.new(gym_params)
+  end
+
+  private
+
+  def gym_params
+    params.require(:gym).permit(:name, :description, :number, :address)
+  end
 end
