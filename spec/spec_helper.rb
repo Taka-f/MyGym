fdbc1dffd6ab22a5b9e0f4dd0a87df0ffdb5549c
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-03-15 15:01:36 +0900

    fix config.yml

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,9 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+  config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
   # rspec-expectations config goes here. You can use an alternate
