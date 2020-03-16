cbef59b2ca227a4bdedb00dd4600c324f02000d0
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-03-16 22:41:23 +0900

    fix config.yml

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -21,45 +21,49 @@
 #     driven_by :selenium_chrome_headless
 #   end
 # end
-# RSpec.configure do |config|
-#   config.before(:each, type: :system) do
-#     driven_by :rack_test
-#   end
-
-#   config.before(:each, type: :system, js: true) do
-#     if ENV["SELENIUM_DRIVER_URL"].present?
-#       driven_by :selenium, using: :chrome,
-#                            options: {
-#                                browser: :remote,
-#                                url: ENV.fetch("SELENIUM_DRIVER_URL"),
-#                                desired_capabilities: :chrome}
-#     else
-#       driven_by :selenium_chrome_headless
-#     end
-#   end
-# end
-# ----------------------------------
-Capybara.server_host = Socket.ip_address_list.detect{|addr| addr.ipv4_private?}.ip_address
-Capybara.server_port = 3000
-
-Capybara.register_driver :selenium_remote do |app|
-  url = "http://chrome:4444/wd/hub"
-  opts = { desired_capabilities: :chrome, browser: :remote, url: url }
-  driver = Capybara::Selenium::Driver.new(app, opts)
-end
-
 RSpec.configure do |config|
-
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_remote
-    host! "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    if ENV["SELENIUM_DRIVER_URL"].present?
+      driven_by :selenium, using: :chrome,
+                           options: {
+                               browser: :remote,
+                               url: ENV.fetch("SELENIUM_DRIVER_URL"),
+                               desired_capabilities: :chrome}
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
-
 end
+# ----------------------------------
+# Capybara.server_host = Socket.ip_address_list.detect{|addr| addr.ipv4_private?}.ip_address
+# Capybara.server_port = 4444
+
+# Capybara.register_driver :selenium_remote do |app|
+#   url = "http://chrome:4444/wd/hub"
+#   opts = { desired_capabilities: :chrome, browser: :remote, url: url }
+#   driver = Capybara::Selenium::Driver.new(app, opts)
+# end
+
+# RSpec.configure do |config|
+
+#   config.before(:each, type: :system) do
+#     driven_by :rack_test
+#   end
+
+#   config.before(:each, type: :system, js: true) do
+#     driven_by :selenium_chrome_headless
+#     # driven_by :selenium_remote
+#     # host! "http://#{Capybara.server_host}:#{Capybara.server_port}"
+#   end
+
+# end
+
+
+
 # require "selenium/webdriver"
 # Capybara.register_driver :selenium_chrome_in_container do |app|
 #   Capybara::Selenium::Driver.new app,
