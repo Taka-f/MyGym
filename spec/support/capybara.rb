4eeeb7ddbbcebde5b7cd92fa3ca12249b9400a97
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-03-16 23:22:07 +0900

    fix config.yml

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -21,6 +21,15 @@
 #     driven_by :selenium_chrome_headless
 #   end
 # end
+Capybara.register_driver :headless_chrome do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << "--headless"
+    opts.args << "--disable-gpu"
+    opts.args << "--no-sandbox"
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by :rack_test
@@ -34,7 +43,8 @@ RSpec.configure do |config|
                                url: ENV.fetch("SELENIUM_DRIVER_URL"),
                                desired_capabilities: :chrome}
     else
-      driven_by :selenium_chrome_headless
+      # driven_by :selenium_chrome_headless
+      driven_by :headless_chrome
     end
   end
 end
