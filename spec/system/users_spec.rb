bde07e7e76d516b6e758625ebe0fd992a87ced2c
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-03-17 14:30:06 +0900

    fix config.yml

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-describe 'Users', type: :system, js: true do
+describe 'Users', type: :system do
   let(:user) { FactoryBot.create(:user) }
   # 新規登録ができること
   it 'create new user' do
-    visit root_path
+    # visit root_path
+    visit new_user_registration_path
     expect {
-      click_on '新規登録'
+      # click_on '新規登録'
       fill_in '名前', with: 'user1'
       fill_in 'メールアドレス', with: 'test@mail.com'
       fill_in 'パスワード', with: 'password', match: :first
@@ -19,7 +20,10 @@ describe 'Users', type: :system, js: true do
   # ユーザー情報を変更また写真を登録できること
   it 'edit user profile' do
     sign_in_as(user)
-    visit edit_user_registration_path
+    visit root_path
+    click_on 'マイページ'
+    click_button 'アカウント編集'
+    # visit edit_user_registration_path
     attach_file "user[image]", "#{Rails.root}/spec/fixtures/test.jpeg"
     fill_in '名前', with: 'test2'
     click_button '更新'
@@ -27,16 +31,14 @@ describe 'Users', type: :system, js: true do
   end
 
   # ユーザーはアカウントを削除できること
-  it 'delete user account', js: true, retry: 5 do
+  it 'delete user account', js: true do
     sign_in_as(user)
     visit edit_user_registration_path
-    wait_for_ajax do
-      click_on 'アカウント削除'
-      expect {
-        expect(page.driver.browser.switch_to.alert.text).to eq "本当によろしいですか?"
-        page.driver.browser.switch_to.alert.accept
-        expect(page).to have_content "アカウントを削除しました。またのご利用をお待ちしております。"
-      }.to change{ User.count }.by(-1)
-    end
+    click_on 'アカウント削除'
+    expect {
+      expect(page.driver.browser.switch_to.alert.text).to eq "本当によろしいですか?"
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content "アカウントを削除しました。またのご利用をお待ちしております。"
+    }.to change{ User.count }.by(-1)
   end
 end
