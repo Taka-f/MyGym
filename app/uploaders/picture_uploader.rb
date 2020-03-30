d762a64b045cb8fdfeb624f7f83ac0cec6cacfaa
Takaaki Fukumitsu <hazikeru.superpopcorn@gmail.com>
2020-03-30 15:06:04 +0900

    fix s3 bucket

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -47,7 +47,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    original_filename if original_filename
+  end
 end
